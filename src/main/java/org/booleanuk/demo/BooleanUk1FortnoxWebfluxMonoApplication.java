d386fd6a113ece14e79e40394b113a9849609bf1
Guybrush3791 <giovanni@wows.dev>
2025-09-03 15:39:59 +0200

    Day 18 - Ex

---
+++ src/main/java/org/booleanuk/demo/BooleanUk1FortnoxWebfluxMonoApplication.java
@@ -0,0 +1,13 @@
+package org.booleanuk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooleanUk1FortnoxWebfluxMonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooleanUk1FortnoxWebfluxMonoApplication.class, args);
+	}
+
+}
